a39f51f9d9bb1ede650acaf1116ccfa493ef5c9f
Emmanuel <ecobian12310@gmail.com>
2025-07-19 12:27:30 -0700

    added cors for mobile functionality

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const port = 3000;
 const userRoutes = require('./routes/users');
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // Routes
 app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
-  res.send('SQLite Hackathon API is live!');
+  res.send('Ice-Breakers Backend API is live!');
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running at http://localhost:${port}`);
 });
